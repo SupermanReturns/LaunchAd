7adb3226eff45cecfc3b2bb870aa53eee1bd9f76
Superman <703353105@qq.com>
2018-07-06 15:10:06 +0800

    Initial Commit

---
+++ LaunchAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaunchAd
+//
+//  Created by Superman on 2018/7/6.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
